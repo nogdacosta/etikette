1b239c38b3ef5132abb2609ca13a44b4c4bb6a1f
Andre Costa <acosta@advicefront.com>
2016-04-10 02:04:53 +0100

    add new layout for login/register views, update basic layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,26 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  # change layout for login, register, forgot password
+  layout :layout_by_resource
+
   protected
-  def authenticate_user!
+
+  def authenticate_user!(options={})
     if user_signed_in?
       super
     else
       redirect_to new_user_session_path #, :notice => 'if you want to add a notice'
       ## if you want render 404 page
       ## render :file => File.join(Rails.root, 'public/404'), :formats => [:html], :status => 404, :layout => false
+    end
+  end
+
+  def layout_by_resource
+    if devise_controller? && resource_name == :user && action_name == "new"
+      "auth"
+    else
+      "application"
     end
   end
 
