7128dfaac5ba7ede150a30615ac39c671631d1de
Andre Costa <acosta@advicefront.com>
2016-05-04 23:35:24 +0100

    Permit avatar for user model

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,6 +3,6 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def account_update_params
-    params.require(:user).permit(:user_name, :email, :password, :password_confirmation, :current_password)
+    params.require(:user).permit(:user_name, :email, :password, :password_confirmation, :current_password, :avatar, :avatar_cache, :remove_avatar)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,21 @@
 class User < ApplicationRecord
+
+  mount_uploader :avatar, AvatarUploader
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates_presence_of   :avatar
+  validates_integrity_of  :avatar
+  validates_processing_of :avatar
+
+  validate :avatar_size_validation
+
+  private
+  def avatar_size_validation
+    errors[:avatar] << "should be less than 2MB" if avatar.size > 10.megabytes
+  end
+
 end
