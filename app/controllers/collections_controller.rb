d6038f4ecc34956ea355c7684bb32986f3b3094d
Andre Costa <acosta@advicefront.com>
2016-08-07 19:32:48 +0100

    Updating views and controllers (eventually bookmarks controller will disappear)

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,5 +1,5 @@
 class CollectionsController < ApplicationController
-  before_action :set_collection, only: [:show, :edit, :update, :destroy]
+  before_action :set_collection, except: :index
 
   before_filter :authenticate_user!
   ## if you want spesific action for require authentication
@@ -8,34 +8,32 @@ class CollectionsController < ApplicationController
   # GET /collections
   # GET /collections.json
   def index
-    @collections = Collection.where(user: current_user)
+    @user = current_user.collection
+    @collections = @user.collections
   end
 
   # GET /collections/1
   # GET /collections/1.json
   def show
+    @collections = @collection.collections
     @items = @collection.bookmarks
   end
 
-  # GET /collections/new
+  # GET /collections/:parent/new
   def new
-    # @collection = Collection.new
-    @collection = current_user.collection.build
-  end
-
-  # GET /collections/1/edit
-  def edit
+    @item = Collection.new
   end
 
   # POST /collections
   # POST /collections.json
-  def create
-    @collection = current_user.collection.build(collection_params)
-    # @collection = Collection.new(collection_params)
+  def save
+    collection = Collection.new(collection_params)
+    @collection.collections << collection
+    @collection.save
 
     respond_to do |format|
       if @collection.save
-        format.html { redirect_to @collection, notice: 'Collection was successfully created.' }
+        format.html { redirect_to collection_path, notice: 'Collection was successfully created.' }
         format.json { render :show, status: :created, location: @collection }
       else
         format.html { render :new }
@@ -44,12 +42,17 @@ class CollectionsController < ApplicationController
     end
   end
 
+  # GET /collections/:parent/1/edit
+  def edit
+  end
+
+
   # PATCH/PUT /collections/1
   # PATCH/PUT /collections/1.json
   def update
     respond_to do |format|
       if @collection.update(collection_params)
-        format.html { redirect_to @collection, notice: 'Collection was successfully updated.' }
+        format.html { redirect_to collection_path, notice: 'Collection was successfully updated.' }
         format.json { render :show, status: :ok, location: @collection }
       else
         format.html { render :edit }
@@ -60,13 +63,16 @@ class CollectionsController < ApplicationController
 
   # DELETE /collections/1
   # DELETE /collections/1.json
-  def destroy
-    @collection.destroy
-    respond_to do |format|
-      format.html { redirect_to collections_url, notice: 'Collection was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+  # def destroy
+  #   @collection.destroy
+  #   respond_to do |format|
+  #     format.html { redirect_to collections_url, notice: 'Collection was successfully destroyed.' }
+  #     format.json { head :no_content }
+  #   end
+  # end
+
+
+  # Bookmark Actions
 
   def new_bookmark
     @collection = Collection.find(params[:collection_id])
@@ -85,11 +91,11 @@ class CollectionsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_collection
-      @collection = Collection.find(params[:id])
+      @collection = Collection.find(params[:collection_id])
     end
 
     def authorized_user
-      @collection = current_user.collections.find_by(id: params[:id])
+      @collection = current_user.collections.find_by(id: params[:collection_id])
       redirect_to collections_path, notice: "Not authorized to edit this collection" if @collection.nil?
     end
 
