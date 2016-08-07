472ced8b54aa05aaeebc914843b8cdaf7dcddd4a
Andre Costa <acosta@advicefront.com>
2016-08-07 20:35:41 +0100

    On user creation we need to create a default collection

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  has_one :collection, as: :parent
+  after_create :generate_default_collection
 
+  has_one :collection, as: :parent
   mount_uploader :avatar, AvatarUploader
 
   # Include default devise modules. Others available are:
@@ -14,7 +15,21 @@ class User < ApplicationRecord
 
   validate :avatar_size_validation, on: :update
 
+
+
   private
+
+  def generate_default_collection
+    collection = Collection.new
+    collection.name = "default"
+    collection.description = "default desc"
+    collection.tags = "default"
+    collection.categories = "default"
+    collection.parent_type = User
+    collection.parent_id = self.id
+    collection.save
+  end
+
   def avatar_size_validation
     errors[:avatar] << "should be less than 2MB" if avatar.size > 10.megabytes
   end
