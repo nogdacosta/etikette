33dd91792373d00288a827514af28fb4f7c01bf8
Andre Costa <acosta@advicefront.com>
2016-08-08 01:23:35 +0100

    using Addressable instead of URI api

--- app/helpers/collections_helper.rb
+++ app/helpers/collections_helper.rb
@@ -1,15 +1,7 @@
 module CollectionsHelper
 
 	def uri_parse string
-		uri = URI::parse(string)
-		if uri.scheme.nil? && uri.host.nil?
-		  unless uri.path.nil?
-		    uri.scheme = "http"
-		    uri.host = uri.path
-		    uri.path = ""
-		  end
-		end
-		uri.to_s
+		Addressable::URI.heuristic_parse(string).to_s
 	end
-	
+
 end
