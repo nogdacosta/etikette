599056a8253ff6395dbe09cf76308c8ac71bc68f
Andre Costa <acosta@advicefront.com>
2016-08-08 01:07:52 +0100

    small method to parse strings into url

--- app/helpers/collections_helper.rb
+++ app/helpers/collections_helper.rb
@@ -1,2 +1,15 @@
 module CollectionsHelper
+
+	def uri_parse string
+		uri = URI::parse(string)
+		if uri.scheme.nil? && uri.host.nil?
+		  unless uri.path.nil?
+		    uri.scheme = "http"
+		    uri.host = uri.path
+		    uri.path = ""
+		  end
+		end
+		uri.to_s
+	end
+	
 end
