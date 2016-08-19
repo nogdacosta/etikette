d66b303722c6d61150fa53e561a4a8f60d1f7935
Andre Costa <acosta@advicefront.com>
2016-08-19 21:38:52 +0100

    hotfix. remove index true from tags name... adding index a bit later on migrations

--- db/migrate/20160812130236_acts_as_taggable_on_migration.acts_as_taggable_on_engine.rb
+++ db/migrate/20160812130236_acts_as_taggable_on_migration.acts_as_taggable_on_engine.rb
@@ -2,7 +2,7 @@
 class ActsAsTaggableOnMigration < ActiveRecord::Migration
   def self.up
     create_table :tags do |t|
-      t.string :name, index: true
+      t.string :name
     end
 
     create_table :taggings do |t|
