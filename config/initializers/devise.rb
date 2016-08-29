027265d3c60ff6835c42165744cf86ea8ead6bea
Andre Costa <acosta@advicefront.com>
2016-08-29 23:53:52 +0100

    Add confirmable to devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,10 +12,10 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.mailer_sender = 'no-reply@etikette.co'
 
   # Configure the class responsible to send e-mails.
-  # config.mailer = 'Devise::Mailer'
+  config.mailer = 'Devise::Mailer'
 
   # ==> ORM configuration
   # Load and configure the ORM. Supports :active_record (default) and
@@ -125,6 +125,8 @@ Devise.setup do |config|
   # db field (see migrations). Until confirmed, new email is stored in
   # unconfirmed_email column, and copied to email column on successful confirmation.
   config.reconfirmable = true
+
+  config.allow_unconfirmed_access_for = 15.days
 
   # Defines which key will be used when confirming an account
   # config.confirmation_keys = [:email]
