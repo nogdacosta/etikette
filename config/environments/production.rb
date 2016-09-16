751ca2145ba80a6b438fa1a36be8a2952705aef7
Andre Costa <acosta@advicefront.com>
2016-09-16 23:57:37 +0100

    hotfix email config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,15 +30,14 @@ Rails.application.configure do
   config.assets.digest = true
 
   # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  #config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default charset: "utf-8"
+  #config.action_mailer.perform_deliveries = true
+  #config.action_mailer.raise_delivery_errors = true
+  #config.action_mailer.default charset: "utf-8"
   config.action_mailer.smtp_settings = {
     authentication: :plain,
     address: ENV["email_provider_address"],
-    enable_starttls_auto: true,
     port: ENV["email_provider_port"],
     domain:  ENV["email_provider_domain"],
     user_name: ENV["email_provider_username"],
